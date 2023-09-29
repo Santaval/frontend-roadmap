5fbd67cd64465238d0df5b1088f2fd4dcb7503aa
Aaron Santana <system@savaldev.com>
2023-09-28 23:44:14 -0600

    nav done

--- users/src/App.js
+++ users/src/App.js
@@ -1,6 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import Navigation from "./components/Navigation";
+
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Navigation />
+      </BrowserRouter>
     </>
   );
 }
--- users/tailwind.config.js
+++ users/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{html,js,jsx}", "./public/**/*.{html,js,jsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#d81159",
+        secondary: "#353535",
+        "text-color": "#000000",
+        "text-color-secondary": "#ffffff",
+        "text-gray": "#8a8a8a",
+      },
+    },
+
   },
   plugins: [],
 }
